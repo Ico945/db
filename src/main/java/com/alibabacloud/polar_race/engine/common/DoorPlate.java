894fbe81c056b7d99e99d807c29b7fed65febd0d
ss <787178638@qq.com>
2018-11-02 19:17:35 +0800

    fix conflict

---
+++ src/main/java/com/alibabacloud/polar_race/engine/common/DoorPlate.java
@@ -0,0 +1,10 @@
+package com.alibabacloud.polar_race.engine.common;
+
+public class DoorPlate {
+    public DoorPlate(String path) {
+
+    }
+    public Location AddOrUpdate(String key) {
+
+    }
+}
--- src/main/java/com/alibabacloud/polar_race/engine/common/EngineRace.java
+++ src/main/java/com/alibabacloud/polar_race/engine/common/EngineRace.java
@@ -1,32 +1,42 @@
-package com.alibabacloud.polar_race.engine.common;
-
-import com.alibabacloud.polar_race.engine.common.AbstractEngine;
-import com.alibabacloud.polar_race.engine.common.exceptions.EngineException;
-import com.alibabacloud.polar_race.engine.common.exceptions.RetCodeEnum;
-
-public class EngineRace extends AbstractEngine {
-
-	@Override
-	public void open(String path) throws EngineException {
-	}
-	
-	@Override
-	public void write(byte[] key, byte[] value) throws EngineException {
-	}
-	
-	@Override
-	public byte[] read(byte[] key) throws EngineException {
-		byte[] value = null;
-		
-		return value;
-	}
-	
-	@Override
-	public void range(byte[] lower, byte[] upper, AbstractVisitor visitor) throws EngineException {
-	}
-	
-	@Override
-	public void close() {
-	}
-
-}
+//package com.alibabacloud.polar_race.engine.common;
+//
+//import com.alibabacloud.polar_race.engine.common.AbstractEngine;
+//import com.alibabacloud.polar_race.engine.common.exceptions.EngineException;
+//import com.alibabacloud.polar_race.engine.common.exceptions.RetCodeEnum;
+//
+//import java.nio.channels.FileLock;
+//
+//public class EngineRace extends AbstractEngine {
+//	FileLock lock = null;
+//	DataStore store = null;
+//	DoorPlate plate = null;
+//	@Override
+//	public void open(String path) throws EngineException {
+//		store = new DataStore(path);
+//		plate = new DoorPlate(path);
+//	}
+//
+//	@Override
+//	public void write(byte[] key, byte[] value) throws EngineException {
+//		// 需要锁
+//		Location location = store.Append(value);
+//		plate.AddOrUpdate(key, location);
+//	}
+//
+//	@Override
+//	public byte[] read(byte[] key) throws EngineException {
+//		byte[] value = null;
+//		Location location = plate.Find(key);
+//		value = store.Read(location);
+//		return value;
+//	}
+//
+//	@Override
+//	public void range(byte[] lower, byte[] upper, AbstractVisitor visitor) throws EngineException {
+//	}
+//
+//	@Override
+//	public void close() {
+//	}
+//
+//}
